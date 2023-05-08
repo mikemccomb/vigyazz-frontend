d900cf9f9ce13d71f353ee53c7ddcd6154d71dc7
mikemccomb <mikemccomb@gmail.com>
2023-05-08 15:46:56 -0500

    Makes form look more form-like

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,13 +1,25 @@
 export function Signup() {
   return (
     <form>
-      <input type="text" name="first_name" id="first_name" />
-      <input type="text" name="last_name" id="last_name" />
-      <input type="email" name="email" id="email" />
-      <input type="password" name="password" id="password" />
-      <input type="password" name="password_confirmation" id="password_confirmation" />
-      <input type="text" name="home_currency" id="home_currency" />
-      <input type="submit" value="" />
+      <div>
+        First name: <input type="text" name="first_name" id="first_name" />
+      </div>
+      <div>
+        Last name: <input type="text" name="last_name" id="last_name" />
+      </div>
+      <div>
+        Email: <input type="email" name="email" id="email" />
+      </div>
+      <div>
+        Password: <input type="password" name="password" id="password" />
+      </div>
+      <div>
+        Confirm Password: <input type="password" name="password_confirmation" id="password_confirmation" />
+      </div>
+      <div>
+        What currency do you use? <input type="text" name="home_currency" id="home_currency" />
+      </div>
+      <input type="submit" value="Sign up" />
     </form>
   );
 }
