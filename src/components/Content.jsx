cd828b90b6529f04eb6238948ba14e7459476eea
mikemccomb <mikemccomb@gmail.com>
2023-05-09 15:53:11 -0500

    Verifies login creates jwt

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -5,10 +5,10 @@ import { Signup } from "./Signup";
 export function Content() {
   return (
     <div>
-      <SearchBar />
+      {/* <SearchBar /> */}
       The weather in [Name of search place] is: [Pull from API]. The current time in [Name of search place] is: [Human
       Time]
-      <Signup />
+      {/* <Signup /> */}
       <Login />
     </div>
   );
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -7,10 +7,36 @@ if (jwt) {
 }
 
 export function Login() {
+  const [errors, setErrors] = useState([]);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setErrors([]);
+    const params = new FormData(event.target);
+    axios
+      .post("http://localhost:3000/sessions.json", params)
+      .then((response) => {
+        console.log(response.data);
+        axios.defaults.headers.common["Authorization"] = "Bearer " + response.data.jwt;
+        localStorage.setItem("jwt", response.data.jwt);
+        event.target.reset();
+        // window.location.href = "/";
+      })
+      .catch((error) => {
+        console.log(error.response);
+        setErrors(["Invalid email or password"]);
+      });
+  };
+
   return (
     <>
       <h2>Log into Vigyazz</h2>
-      <form onSubmit="">
+      <ul>
+        {errors.map((error) => (
+          <li key={error}>{error}</li>
+        ))}
+      </ul>
+      <form onSubmit={handleSubmit}>
         <div>
           Email: <input type="email" name="email" id="email-login" />
         </div>
