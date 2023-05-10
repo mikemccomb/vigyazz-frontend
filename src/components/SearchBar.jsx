ababafa12f7f598535ccccc5a0b5cedde11793ac
mikemccomb <mikemccomb@gmail.com>
2023-05-10 16:11:41 -0500

    Builds search functionality

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,19 +1,27 @@
-import { useEffect, useState } from "react";
+// import { useState } from "react";
 import axios from "axios";
 
 export function SearchBar() {
-  const [searchFilter, setSearchFilter] = useState("");
+  // const [searchFilter, setSearchFilter] = useState("");
 
-  useEffect(() => {
-    axios.get(`http://localhost:3000/weather`).then((response) => console.log(response.data));
-  });
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("1st");
+    const params = new FormData(event.target);
+    axios
+      .get("http://localhost:3000/weather.json", { params: { search: params.get("search") } })
+      .then((response) => console.log("Yo", response.data));
+    console.log("2nd");
+  };
 
   return (
     <div>
-      <div>
-        Search:{" "}
-        <input type="search" value={searchFilter} list="" onChange={(event) => setSearchFilter(event.target.value)} />
-      </div>
+      <form onSubmit={handleSubmit}>
+        <div>
+          Enter a city: <input type="text" name="search" />
+        </div>
+        <button type="submit">Search</button>
+      </form>
     </div>
   );
 }
